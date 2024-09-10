66e92de9b2809eca989d3a3a798facdcbc20667b
brewost <156411525+brewost@users.noreply.github.com>
2024-09-10 13:34:03 +0200

    04delete done

--- src/app.js
+++ src/app.js
@@ -20,10 +20,14 @@ app.get("/api/movies/:id", movieControllers.getMovieById);
 app.post("/api/movies", validateMovie, movieControllers.postMovie);
 app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
 
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
+
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
 
 app.post("/api/users", validateUser, userControllers.postUser);
 app.put("/api/users/:id", validateUser, userControllers.updateUser);
+
+app.delete("/api/users/:id", userControllers.deleteUser);
 
 module.exports = app;
