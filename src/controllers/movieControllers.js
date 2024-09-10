0d8177fa1e6128cd28d3c9db8ac209bc2fdb1343
brewost <156411525+brewost@users.noreply.github.com>
2024-09-10 09:33:13 +0200

    01.2bis done

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -1,58 +1,41 @@
 const database = require("../../database");
 
 const getMovies = (req, res) => {
-
   database
 
     .query("select * from movies")
 
     .then(([movies]) => {
-
       res.json(movies);
-
     })
 
     .catch((err) => {
-
       console.error(err);
 
       res.sendStatus(500);
-
     });
-
 };
 
 const getMovieById = (req, res) => {
-
   const id = parseInt(req.params.id);
-
 
   database
 
     .query("select * from movies where id = ?", [id])
 
     .then(([movies]) => {
-
       if (movies[0] != null) {
-
         res.json(movies[0]);
-
       } else {
-
         res.sendStatus(404);
-
       }
-
     })
 
     .catch((err) => {
-
       console.error(err);
 
       res.sendStatus(500);
-
     });
-
 };
 
 module.exports = {
--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,58 +1,57 @@
 const database = require("../../database");
 
 const getUsers = (req, res) => {
+  // Retrieve query parameters for language and city
+  const { language, city } = req.query;
 
+  // Base SQL query
+  let query = "SELECT * FROM users";
+  const queryParams = [];
+
+  // Add conditions based on query parameters
+  if (language) {
+    query += " WHERE language = ?";
+    queryParams.push(language);
+  }
+
+  if (city) {
+    // Check if 'language' is already added to avoid multiple WHERE clauses
+    if (queryParams.length > 0) {
+      query += " AND city = ?";
+    } else {
+      query += " WHERE city = ?";
+    }
+    queryParams.push(city);
+  }
+
+  // Execute the SQL query
   database
-
-    .query("select * from users")
-
+    .query(query, queryParams)
     .then(([users]) => {
-
       res.json(users);
-
     })
-
     .catch((err) => {
-
       console.error(err);
-
       res.sendStatus(500);
-
     });
-
 };
 
 const getUserById = (req, res) => {
-
   const id = parseInt(req.params.id);
 
-
   database
-
-    .query("select * from users where id = ?", [id])
-
+    .query("SELECT * FROM users WHERE id = ?", [id])
     .then(([users]) => {
-
       if (users[0] != null) {
-
         res.json(users[0]);
-
       } else {
-
         res.sendStatus(404);
-
       }
-
     })
-
     .catch((err) => {
-
       console.error(err);
-
       res.sendStatus(500);
-
     });
-
 };
 
 module.exports = {
